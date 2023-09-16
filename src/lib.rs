07832efee7a05c164ba9fcc8f813ea650cae5a13
Hiroyuki Tachibana <hiroism007@gmail.com>
2023-09-16 21:33:40 +0900

    wip: main circuit structure
    chore: change text

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,67 @@ pub mod signature_verification;
 
 use crate::signature_verification::*;
 
+use halo2_base::halo2_proofs::circuit;
+use halo2_base::halo2_proofs::circuit::{SimpleFloorPlanner, Value};
+use halo2_base::halo2_proofs::plonk::{Circuit, Column, ConstraintSystem, Instance};
+use halo2_base::halo2_proofs::{circuit::Layouter, plonk::Error};
+use halo2_base::utils::fe_to_biguint;
+use halo2_base::utils::{decompose_fe_to_u64_limbs, value_to_option};
+use halo2_base::QuantumCell;
+use halo2_base::{gates::range::RangeStrategy::Vertical, SKIP_FIRST_PASS};
+use halo2_base::{
+    gates::{flex_gate::FlexGateConfig, range::RangeConfig, GateInstructions, RangeInstructions},
+    utils::PrimeField,
+};
+
+pub use halo2_rsa;
+use halo2_rsa::*;
+use rsa::PublicKeyParts;
+use sha2::{Digest, Sha256};
+use std::io::{Read, Write};
+use std::marker::PhantomData;
+use num_bigint::BigUint;
+use serde::{Deserialize, Serialize};
+
+pub const VERIFY_CONFIG_ENV: &'static str = "VERIFY_CONFIG";
+
+/// Configuration for [`DefaultMynaCircuit`].
+#[derive(Debug, Clone)]
+pub struct DefaultMynaConfig<F: PrimeField> {
+    pub sign_verify_config: SignatureVerificationConfig<F>,
+    pub instances: Column<Instance>,
+}
+
+#[derive(Debug, Clone)]
+pub struct DefaultMynaCircuit<F: PrimeField> {
+    pub hashed: Vec<u8>, // A SHA256 hashed message
+    pub signature: Vec<u8>, // A signature
+    pub public_key_n: BigUint, // pub public_key: RSAPublicKey<F>,
+    _f: PhantomData<F>,
+}
+
+impl<F: PrimeField> Circuit<F> for DefaultMynaCircuit<F> {
+    type Config = DefaultMynaConfig<F>;
+    type FloorPlanner = SimpleFloorPlanner;
+
+    fn without_witnesses(&self) -> Self {
+        Self {
+            hashed: vec![],
+            signature: vec![],
+            public_key_n: self.public_key_n.clone(),
+            _f: PhantomData,
+        }
+    }
+
+    fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
+        todo!()
+    }
+
+    fn synthesize(&self, config: Self::Config, layouter: impl Layouter<F>) -> Result<(), Error> {
+        todo!()
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
