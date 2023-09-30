75e43082f96a3f8bad2be6fae22e40388381e360
masatsch <kingmasatojames@gmail.com>
2023-09-30 13:31:01 +0900

    wip: build snark-verifier-sdk

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,19 @@ pub mod signature_verification;
 use crate::signature_verification::*;
 
 use halo2_base::halo2_proofs::circuit::{SimpleFloorPlanner, Value};
-use halo2_base::halo2_proofs::plonk::{Circuit, Column, ConstraintSystem, Instance};
+use halo2_base::halo2_proofs::dev::MockProver;
+use halo2_base::halo2_proofs::halo2curves::bn256::{Fq, Fr, Bn256, G1Affine};
+use halo2_base::halo2_proofs::plonk::{
+    Circuit, Column, ConstraintSystem, Instance, Selector,
+    // create_proof, keygen_pk, keygen_vk, verify_proof, ProvingKey, VerifyingKey,
+};
+use halo2_base::halo2_proofs::poly::{
+    commitment::{Params, Prover},
+    kzg::{
+        commitment::{KZGCommitmentScheme, ParamsKZG},
+        multiopen::{ProverGWC, VerifierGWC},
+    },
+};
 use halo2_base::halo2_proofs::{circuit::Layouter, plonk::Error};
 use halo2_base::AssignedValue;
 use halo2_base::{gates::range::RangeStrategy::Vertical, SKIP_FIRST_PASS};
@@ -16,6 +28,7 @@ use halo2_base::halo2_proofs::dev::CellValue::Assigned;
 pub use halo2_rsa;
 use halo2_rsa::*;
 use num_bigint::BigUint;
+use rand::rngs::OsRng;
 use std::marker::PhantomData;
 
 pub const VERIFY_CONFIG_ENV: &str = "VERIFY_CONFIG";
@@ -146,15 +159,25 @@ impl<F: PrimeField> DefaultMynaCircuit<F> {
 
 #[cfg(test)]
 mod test {
+    use std::path::Path;
+
     use super::*;
     use halo2_base::halo2_proofs::dev::MockProver;
     use halo2_base::halo2_proofs::halo2curves::bn256::Fr;
 
+    use halo2_base::utils::fs::gen_srs;
     use num_bigint::BigUint;
     use rand::thread_rng;
     use rand::Rng;
     use rsa::{Hash, PaddingScheme, PublicKeyParts, RsaPrivateKey, RsaPublicKey};
     use sha2::{Digest, Sha256};
+    use halo2_base::halo2_proofs::poly::commitment::Params;
+    use snark_verifier_sdk::{
+        halo2::{gen_proof, gen_snark_shplonk},
+        gen_pk,
+        Snark,
+        CircuitExt,
+    };
 
     #[test]
     fn test_signature_verification() {
@@ -218,5 +241,73 @@ mod test {
             DefaultMynaCircuit::<Fr>::new(hashed_msg.to_vec(), sign.to_vec(), public_key_n);
         let prover = MockProver::run(19, &circuit, vec![public_inputs]).unwrap();
         assert_eq!(prover.verify(), Ok(()));
+    }
+
+    #[test]
+    fn test_verify_evm() {
+        let bits = 2048;
+
+        // 1. Generate a key pair.
+        let mut rng = thread_rng();
+        let private_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
+        let public_key = RsaPublicKey::from(&private_key);
+
+        // 2. Uniformly sample a message.
+        let mut msg: [u8; 128] = [0; 128];
+        for x in &mut msg[..] {
+            *x = rng.gen();
+        }
+
+        // 3. Compute the SHA256 hash of `msg`.
+        let hashed_msg = Sha256::digest(msg);
+
+        // 4. Generate a pkcs1v15 signature.
+        let padding = PaddingScheme::PKCS1v15Sign {
+            hash: Some(Hash::SHA2_256),
+        };
+        let sign = private_key
+            .sign(padding, &hashed_msg)
+            .expect("fail to sign a hashed message.");
+
+        let public_key_n = BigUint::from_bytes_be(&public_key.n().clone().to_bytes_be());
+
+        // 公開鍵をVec<Fr>に変換
+        let pub_key_vec = public_key
+            .n()
+            .clone()
+            .to_bytes_be()
+            .iter()
+            .map(|v| Fr::from(*v as u64))
+            .collect::<Vec<Fr>>();
+
+        // 署名をVec<Fr>に変換
+        let sign_vec = sign
+            .to_vec()
+            .iter()
+            .map(|v| Fr::from(*v as u64))
+            .collect::<Vec<Fr>>();
+
+        // ハッシュされたメッセージをVec<Fr>に変換
+        let hashed_vec = hashed_msg
+            .iter()
+            .map(|v| Fr::from(*v as u64))
+            .collect::<Vec<Fr>>();
+
+        // 公開鍵、署名、ハッシュされたメッセージを結合
+        let public_inputs = pub_key_vec
+            .iter()
+            .chain(sign_vec.iter())
+            .chain(hashed_vec.iter())
+            .cloned()
+            .collect::<Vec<Fr>>();
+
+        let circuit =
+            DefaultMynaCircuit::<Fr>::new(hashed_msg.to_vec(), sign.to_vec(), public_key_n);
+
+        let params = gen_srs(8);
+        let pk = gen_pk(&params, &circuit, Some(Path::new("app.pk")));
+        let snark = gen_snark_shplonk(&params, &pk, circuit, Some(Path::new("snark")));
+        let proof = snark.proof();
+        assert!(proof);
     }
 }
